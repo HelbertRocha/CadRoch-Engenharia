b3747626b1187c576147e192e22052e81d908580
Anders Opstrup <aopstrup@gmail.com>
2015-09-15 09:31:33 -0300

    homeView and createNewUserView v1 finished

--- www/tests/integrationTests/createNewUserView.tests.js
+++ www/tests/integrationTests/createNewUserView.tests.js
@@ -13,10 +13,34 @@ describe('integration test of home view functionality', function() {
     element(by.id('newUserButton')).click();
   });
 
-  it("have a username field", function () {
+  it("should have a username field", function () {
 
       var usernameInputField = element(by.id('usernameModelView'));
       expect(usernameInputField.isPresent()).toBeTruthy();
+  });
+
+  it("should have a name field", function () {
+
+    var nameInputField = element(by.id('nameModelView'));
+    expect(nameInputField.isPresent()).toBeTruthy();
+  });
+
+  it("should have a surname field", function () {
+
+    var surnameInputField = element(by.id('surnameModelView'));
+    expect(surnameInputField.isPresent()).toBeTruthy();
+  });
+
+  it("should have a take picture button", function () {
+
+    var pictureButton = element(by.id('pictureButtonModelView'));
+    expect(pictureButton.isPresent()).toBeTruthy();
+  });
+
+  it("should have a create user button", function () {
+
+    var createButton = element(by.id('createButtonModelView'));
+    expect(createButton.isPresent()).toBeTruthy();
   });
 });
 
